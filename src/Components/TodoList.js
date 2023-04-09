b0e31cfae7137b11cfac399d63c620f5fd8e9c4a
MD Rezwan Hassan Reshad <Reshad>
2023-04-09 07:02:32 +0600

    finish all logic

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -16,7 +16,19 @@ const TodoList = () => {
             }))
 
     }
+
+    const removeActivity = function removeActivity(i){
+        const updateData = listData.filter((element , id)=>{
+            return i!=id;
+        })
+        setListData(updateData)
+    }
+
+    const removeAll = function removeAll(){
+        setListData([])
+    }
     return (
+        <>
         <div className='container'>
             <div className="header">ToDo-List</div>
             <input type="text" placeholder='Add Activity' value={Activity} onChange={(e) =>{
@@ -24,6 +36,25 @@ const TodoList = () => {
             }}/>
             <button onClick={addActivity}>Add</button>
         </div>
+
+        <div className='container2'>
+        <p className='List-heading'>Here is your list:</p>
+        {listData !==[] && listData.map((data,i)=>{
+            return(
+                <>
+                <p key={i}>
+                    <div className="listData">{data}</div>
+                    <div className="btn-position">
+                        <button onClick={()=>removeActivity(i)}>Remove(-)</button>
+                    </div>
+                </p>
+                </>
+            )
+        })}
+        {listData.length>=1 && <button onClick={removeAll}>Remove All</button>}
+        </div>
+        </>
+
     );
 };
 
